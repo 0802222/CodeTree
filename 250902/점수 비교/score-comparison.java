4684c19102cf73d6e98194ae2effbbba85900022
이초롱 <132190391+0802222@users.noreply.github.com>
2025-09-02 18:53:12 +0900

    Title: 점수 비교, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/점수 비교/score-comparison.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        int aMath = sc.nextInt();
+        int aEng = sc.nextInt();
+        int bMath = sc.nextInt();
+        int bEng = sc.nextInt();
+
+        int result = (aMath > bMath && aEng > bEng) ? 1 : 0;
+        System.out.println(result);
+    }
+}
