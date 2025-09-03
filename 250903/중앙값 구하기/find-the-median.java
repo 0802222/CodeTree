0b725bc2c4e90355999141838cb44d43f4c9766b
이초롱 <132190391+0802222@users.noreply.github.com>
2025-09-03 14:04:23 +0900

    Title: 중앙값 구하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250903/중앙값 구하기/find-the-median.java
+++ 250903/중앙값 구하기/find-the-median.java
@@ -14,10 +14,14 @@ public class Main {
         if (a > b) {
             if (a < c) {
                 System.out.println(a);
+            } else {
+                System.out.println(c);
             }
         } else if (b > a) {
             if (b < c) {
                 System.out.println(b);
+            } else {
+                System.out.println(c);
             }
         } else {
             System.out.println(c);
