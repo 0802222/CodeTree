288c8c60af288b7dcc6d3e6e2ca19df267e6aed2
이초롱 <132190391+0802222@users.noreply.github.com>
2025-09-30 23:19:29 +0900

    Title: 달에서 무게 구하기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250930/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int weight = 13;
+        double rate = 0.165;
+        double result = weight * rate;
+
+        System.out.printf("%d * %.6f = %.6f", weight, rate, result);
+    }
+}
