d825faffcb60db3761dbfcc9fe28457c0d71049c
이초롱 <132190391+0802222@users.noreply.github.com>
2025-10-07 04:28:44 +0900

    Title: 두 사람, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251007/두 사람/two-person.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int aAge = sc.nextInt();
+        String aGender = sc.next();
+        int bAge = sc.nextInt();
+        String bGender = sc.next();
+
+        if ((aAge >= 19 && aGender.equals("M")) || (bAge >= 19 && bGender.equals("M"))) {
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+
+        sc.close();
+    }
+}
