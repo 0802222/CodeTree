65e02dbd14e7dbeae69bfa014dafeebce6961f07
이초롱 <132190391+0802222@users.noreply.github.com>
2025-10-07 03:51:25 +0900

    Title: 특정 조건 세 정수 비교, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251007/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min = Math.min(a, Math.min(b, c));
+
+        if (a == min) {
+            System.out.print(1 + " ");
+        } 
+        else {
+            System.out.print(0 + " ");
+        }
+
+        if (a == b && b == c) {
+            System.out.print(1 + " ");
+        } 
+        else {
+            System.out.print(0 + " ");
+        }
+
+        sc.close();
+    }
+}
