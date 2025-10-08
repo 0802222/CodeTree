350cb0b28f98d08864e46d3aa334220e9df5f861
이초롱 <132190391+0802222@users.noreply.github.com>
2025-10-09 08:11:34 +0900

    Title: 별 N개 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251009/별 N개 출력하기/print-n-stars.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1;
+        int n = sc.nextInt();
+
+        while (i <= n) {
+            System.out.println("*");
+            i++;
+        }
+    }
+}
