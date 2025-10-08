774d771acf1d880efcbe0f8d08c91895f2e3a666
이초롱 <132190391+0802222@users.noreply.github.com>
2025-10-08 13:03:22 +0900

    Title: 중앙값 구하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251008/중앙값 구하기/find-the-median.java
+++ 251008/중앙값 구하기/find-the-median.java
@@ -12,16 +12,9 @@ public class Main {
 
         int mid = 0;
 
-        if(a > b) {
-            if (a > c) mid = c;
-            else if (a < c) mid = a;
-        } else if (b > a) {
-            if (b > c) mid = c;
-            else if (b < c) mid = b;
-        } else if (c > a) {
-            if (c > b) mid = b;
-            else if (c < b) mid = c;
-        }
+        if((a >= b && a <= c) || (a <= b && a >= c)) mid = a;
+        else if ((b >= a && b <= c) || (b <= a && b >= c)) mid = b;
+        else mid = c;
 
         System.out.println(mid);
 
