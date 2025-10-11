8be0f82b14768a7350c7a3feae29d7a7cfe8776f
이초롱 <132190391+0802222@users.noreply.github.com>
2025-10-11 22:31:33 +0900

    Title: A/B 출력, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251011/A/B 출력/a-divide-b.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // BigDecimal로 변환
+        BigDecimal bdA = new BigDecimal(a);
+        BigDecimal bdB = new BigDecimal(b);
+
+        BigDecimal result = bdA.divide(bdB, 20, RoundingMode.FLOOR);
+
+        System.out.println(result.toPlainString());
+        sc.close();
+    }
+}
