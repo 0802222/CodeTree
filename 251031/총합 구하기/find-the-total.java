9c86765bb5cffdaf6bd29f9ec95e8f5286da7fce
이초롱 <132190391+0802222@users.noreply.github.com>
2025-10-31 02:35:06 +0900

    Title: 총합 구하기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251031/총합 구하기/find-the-total.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+
+        for (int i = min; i <= max; i++) {
+            if (i % 6 == 0 && i % 8 != 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
