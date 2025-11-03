3543fceee733271f945f91329d3ed07a098a7700
이초롱 <132190391+0802222@users.noreply.github.com>
2025-11-03 15:55:52 +0900

    Title: 1부터의 합, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251103/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+        int i;
+
+        for (i = 1; i <= 100; i++) {
+            sum += i;
+            
+            if (sum >= n) {
+                break;
+            }
+        }
+        System.out.print(i);
+        sc.close();
+    }
+}
