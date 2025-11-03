a8ced598d1fa467396ed462c0a531a8b0d421223
이초롱 <132190391+0802222@users.noreply.github.com>
2025-11-04 03:00:43 +0900

    Title: 1부터 곱셈하기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251104/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int prod = 1;
+        int i;
+        
+        for (i = 1; i < 10; i++) {
+            prod *= i;
+
+            if (prod >= n) {
+                break;
+            }
+        }
+        System.out.print(i);
+        sc.close();
+    }
+}
