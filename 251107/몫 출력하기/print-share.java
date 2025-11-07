d5d05c3128c16d24e5da7b0380090beda7d0b695
이초롱 <132190391+0802222@users.noreply.github.com>
2025-11-07 18:49:38 +0900

    Title: 몫 출력하기, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251107/몫 출력하기/print-share.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+
+        while (count < 3) {
+            int n = sc.nextInt();
+
+            if (n % 2 == 1) {
+                continue;
+            } else if (n % 2 == 0) {
+                System.out.println(n / 2);
+                count++;
+            }
+        }
+        sc.close();
+    }
+}
