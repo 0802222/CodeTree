893dc7364b79d47497a7d2a8a56992684c88a2fc
이초롱 <132190391+0802222@users.noreply.github.com>
2025-11-07 19:31:04 +0900

    Title: 20대의 평균 나이, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251107/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            int age = sc.nextInt();
+
+            if (age >= 20 && age <= 29) {
+                sum += age;
+                count++;
+            } else {
+                double avg = (double)sum / count;
+                System.out.printf("%.2f", avg);
+                break;
+            }
+        }
+        sc.close();
+    }
+}
