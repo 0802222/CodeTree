352696cc0ccb4b9d185ad7415409dfab9fba9064
이초롱 <132190391+0802222@users.noreply.github.com>
2026-04-09 18:08:35 +0900

    Title: N개의 정수 중 최대 2개, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/N개의 정수 중 최대 2개/two-max-of-n-num.java
+++ 260409/N개의 정수 중 최대 2개/two-max-of-n-num.java
@@ -13,9 +13,18 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < arr.length -1; i++) {
+		    for (int j = 0; j < arr.length -1 -i; j++) {
+				if (arr[j] < arr[j + 1]) {
+				// 내림차순 : 앞이 더 작으면 swqp
+						int tmp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = tmp;
+				}
+		    }
+        }
 
-        System.out.print(arr[n - 1] + " " + arr[n - 2]);
+        System.out.print(arr[0] + " " + arr[1]);
         
         sc.close();
     }
