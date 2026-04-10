e40b3b195a2fb1a97cf5445bf2c000a850e3e372
이초롱 <132190391+0802222@users.noreply.github.com>
2026-04-10 18:27:41 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260410/중복되지 않는 정수 중 최대/max-of-unique-number.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] cnt = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : arr) {
+            cnt[x]++;
+        }
+
+        int answer = -1;
+
+        for (int x : arr) {
+            if (cnt[x] == 1) {
+                if (answer == -1 || x > answer) {
+                    answer = x;
+                }
+            }
+        }
+        System.out.println(answer);
+        sc.close();
+    }
+}
