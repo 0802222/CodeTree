8710150e0c7f0035cea83bcdef8dbb200ba012d5
이초롱 <132190391+0802222@users.noreply.github.com>
2026-04-26 02:33:51 +0900

    Title: 길이 단위 변환하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260426/길이 단위 변환하기/change-length-unit.py
@@ -0,0 +1,8 @@
+ft = 30.48
+mi = 160934
+
+#print("%.1fft = %.1fcm" % (9.2, 9.2 * ft))
+#print("%.1fmi = %.1fcm" % (1.3, 1.3 * mi))
+
+print(f"9.2ft = {9.2 * ft:.1f}cm")
+print(f"1.3mi = {1.3 * mi:.1f}cm")
