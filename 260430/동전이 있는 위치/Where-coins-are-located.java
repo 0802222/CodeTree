cc212cb461baa0a9e898e0e6dc45f67afea19487
이초롱 <132190391+0802222@users.noreply.github.com>
2026-04-30 21:06:19 +0900

    [Passed] 동전이 있는 위치 (146ms, 10MB)

---
+++ 260430/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] grid = new int[n][n];
+
+        for (int i = 0; i < m; i++) {
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+
+            grid[r - 1][c - 1] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(grid[i][j]);
+                if (j != n - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
