db35b9dea8576e3e8d24a88234faaa37e4916d57
이초롱 <132190391+0802222@users.noreply.github.com>
2026-05-15 16:36:19 +0900

    [Passed] 두 정수 값 교환하기 (129ms, 10MB)

--- trail2/두 정수 값 교환하기/to-exchange-two-integer-values.java
+++ trail2/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
 
+class IntWrapper {
+    int value;
+
+    public IntWrapper(int value) {
+        this.value = value;
+    }
+}
+
 public class Main {
-    public static void change(int n, int m) {
-        int temp = n;
-        n = m;
-        m = temp;
-        System.out.println(n + " " + m);
+    public static void change(IntWrapper n, IntWrapper m) {
+        int temp = n.value;
+        n.value = m.value;
+        m.value = temp;
     }
     
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        IntWrapper n = new IntWrapper(sc.nextInt());
+        IntWrapper m = new IntWrapper(sc.nextInt());
 
         change(n, m);
+        System.out.println(n.value + " " + m.value);
+
         sc.close();
     }
 }
