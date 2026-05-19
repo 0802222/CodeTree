174c0610eddbde3b0e9e49434ac6e271ed089923
이초롱 <132190391+0802222@users.noreply.github.com>
2026-05-19 16:22:22 +0900

    [Passed] 두 수의 거듭제곱 (150ms, 10MB)

---
+++ trail2/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int pow(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(pow(a, b));
+        sc.close();
+    }
+}
