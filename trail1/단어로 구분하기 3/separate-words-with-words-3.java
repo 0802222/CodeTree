9e7e194f81885bd98317b8188d8ef3331fd6327e
이초롱 <132190391+0802222@users.noreply.github.com>
2026-06-04 03:28:19 +0900

    [Passed] 단어로 구분하기 3 (125ms, 10MB)

---
+++ trail1/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = 10;
+        String[] arr = new String[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+        sc.close();
+    }
+}
