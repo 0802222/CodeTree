43ce377e4b5cf67d0281fdf38e68bc13eeaaa577
이초롱 <132190391+0802222@users.noreply.github.com>
2026-06-28 23:53:24 +0900

    [Passed] 문자열 밀기 (127ms, 10MB)

---
+++ trail1/문자열 밀기/push-char.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+        int len = str.length();
+
+        str = str.substring(1) + str.substring(0, 1);
+
+        System.out.print(str);
+        sc.close();
+    }
+}
+
