4f6438b45cc7f5a1ed871e581d4f63be77f7d183
이초롱 <132190391+0802222@users.noreply.github.com>
2026-07-06 23:51:36 +0900

    [Passed] 그 전 알파벳 (137ms, 10MB)

---
+++ trail1/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        char next = (char)(c - 1);
+
+        if(c == 'a') {
+            next = 'z';
+        }
+
+        System.out.println(next);
+
+        sc.close();
+    }
+}
